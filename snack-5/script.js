0b35435d961928aecd2ec16950369fb3916ce2bd
Corrado.Ferra <corradoferra@gmail.com>
2021-12-15 21:59:18 +0100

    snack-5 + bug che non riesco a risolvere

---
+++ snack-5/script.js
@@ -0,0 +1,17 @@
+let array = []
+let sommaNumeri = 0
+
+do{
+    numeri = prompt("Inserisci numero")
+    array.push(numeri)
+    sommaNumeri += parseInt(array)
+    console.log("aggiungi un altro numero")
+}while(sommaNumeri < 50)
+
+console.log(sommaNumeri)
+
+if (sommaNumeri > 50){
+    console.log("la somma supera le 50 unità")
+}else{
+    console.log("Good Job")
+}
